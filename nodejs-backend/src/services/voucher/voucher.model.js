ed4f2b843c605ffd0ba2523b629cb5860bcbe584
Code Bridge Machine <ahmed@codebridge.my>
2026-05-02 10:04:34 +0800

    Code Bridge commit

---
+++ nodejs-backend/src/services/voucher/voucher.model.js
@@ -0,0 +1,21 @@
+
+    module.exports = function (app) {
+        const modelName = "voucher";
+        const mongooseClient = app.get("mongooseClient");
+        const { Schema } = mongooseClient;
+        const schema = new Schema(
+          {
+            name: { type:  String , comment: "Name, p, false, true, true, true, true, true, true, , , , ," },
+total: { type: Boolean, required: false, comment: "Total, p_boolean, false, true, true, true, true, true, true, , , , ," },
+
+            createdBy: { type: Schema.Types.ObjectId, ref: "users", required: true },
+            updatedBy: { type: Schema.Types.ObjectId, ref: "users", required: true },
+          }, { timestamps: true });
+      
+       
+        if (mongooseClient.modelNames().includes(modelName)) {
+          mongooseClient.deleteModel(modelName);
+        }
+        return mongooseClient.model(modelName, schema);
+        
+      };
---
+++ nodejs-backend/test/services/voucher.test.js
@@ -0,0 +1,93 @@
+const assert = require("assert");
+const app = require("../../src/app");
+
+let usersRefData = [
+  {
+    name: "Standard User",
+    email: "standard@example.com",
+    password: "password",
+  },
+];
+
+describe("voucher service", async () => {
+  let thisService;
+  let voucherCreated;
+  let usersServiceResults;
+  let users;
+
+  
+
+  beforeEach(async () => {
+    thisService = await app.service("voucher");
+
+    // Create users here
+    usersServiceResults = await app.service("users").Model.create(usersRefData);
+    users = {
+      createdBy: usersServiceResults[0]._id,
+      updatedBy: usersServiceResults[0]._id,
+    };
+  });
+
+  after(async () => {
+    if (usersServiceResults) {
+      await Promise.all(
+        usersServiceResults.map((i) =>
+          app.service("users").Model.findByIdAndDelete(i._id)
+        )
+      );
+    }
+  });
+
+  it("registered the service", () => {
+    assert.ok(thisService, "Registered the service (voucher)");
+  });
+
+  describe("#create", () => {
+    const options = {"name":"new value","total":true};
+
+    beforeEach(async () => {
+      voucherCreated = await thisService.Model.create({...options, ...users});
+    });
+
+    it("should create a new voucher", () => {
+      assert.strictEqual(voucherCreated.name, options.name);
+assert.strictEqual(voucherCreated.total, options.total);
+assert.strictEqual(voucherCreated.total, options.total);
+    });
+  });
+
+  describe("#get", () => {
+    it("should retrieve a voucher by ID", async () => {
+      const retrieved = await thisService.Model.findById(voucherCreated._id);
+      assert.strictEqual(retrieved._id.toString(), voucherCreated._id.toString());
+    });
+  });
+
+  describe("#update", () => {
+    const options = {"name":"updated value","total":false};
+
+    it("should update an existing voucher ", async () => {
+      const voucherUpdated = await thisService.Model.findByIdAndUpdate(
+        voucherCreated._id, 
+        options, 
+        { new: true } // Ensure it returns the updated doc
+      );
+      assert.strictEqual(voucherUpdated.name, options.name);
+assert.strictEqual(voucherUpdated.total, options.total);
+assert.strictEqual(voucherUpdated.total, options.total);
+    });
+  });
+
+  describe("#delete", async () => {
+    it("should delete a voucher", async () => {
+      await app
+        .service("users")
+        .Model.findByIdAndDelete(usersServiceResults._id);
+
+      ;
+
+      const voucherDeleted = await thisService.Model.findByIdAndDelete(voucherCreated._id);
+      assert.strictEqual(voucherDeleted._id.toString(), voucherCreated._id.toString());
+    });
+  });
+});
